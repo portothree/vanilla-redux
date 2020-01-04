78aa11c9a7a39c1f51244c10379430ba1be6e137
Gustavo Porto <portothree@gmail.com>
2020-01-04 18:56:10 +0000

    :sparkles: redux implementation

--- arrival/main.js
+++ arrival/main.js
@@ -1,8 +1,30 @@
 const arrivalP = document.querySelector(".arrival");
 const usernameInp = document.querySelector(".username");
 
+function arrivalReducer(state = {}, action) {
+	switch (action.type) {
+		case "CHANGE_ARRIVAL":
+			// There's no need to actually copy the state here since
+			// we are only dealing with one entrie ('arrival') in the object.
+			return { ...state, arrival: action.arrival };
+		default:
+			return state;
+	}
+}
+
+const store = Redux.createStore(arrivalReducer);
+
 usernameInp.addEventListener("input", e => {
 	const name = e.target.value;
 
-	arrivalP.innerText = `${name} in the house!`;
+	store.dispatch({
+		type: "CHANGE_ARRIVAL",
+		arrival: `${name} in the house! Welcome.`
+	});
+});
+
+store.subscribe(() => {
+	const state = store.getState();
+
+	arrivalP.innerText = state.arrival;
 });
